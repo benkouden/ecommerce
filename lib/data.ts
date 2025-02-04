fa6ecaca8f7f2d66ec88d0d66154640b40967c6c
benkouden <mahuton02@gmail.com>
2025-02-04 18:09:22 +0100

    03-create-website-layout

---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+    headerMenus:[
+        {
+            name:"Today's Deal",
+            href:"/search?tag=todays-deal",
+        },
+        {
+            name:"New Arrivals",
+            href:"/search?tag=new-arrivals",
+        },
+        {
+            name:"Featured Products",
+            href:"/search?tag=featured-products",
+        },
+        {
+            name:"Best Sellers",
+            href:"/search?tag=best-sellers",
+        },
+        {
+            name:"Browsing History",
+            href:"/#browsing-history",
+        },
+        {
+            name:"Customer Services",
+            href:"/page/customer-services",
+        },
+        {
+            name:"About Us",
+            href:"/page/about-us",
+        },
+        {
+            name:"Help",
+            href:"/page/help",
+        }
+    ],
+}
+export default data
